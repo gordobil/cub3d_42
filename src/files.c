438dc3e3822482939dc4fd8fb0d8fa5263c4f337
gordobil <noahgordobil@gmail.com>
2025-04-23 12:53:22 +0200

    map

--- src/files.c
+++ src/files.c
@@ -98,22 +98,29 @@ int	check_file(t_cub3d *cub3d)
 
 	while (1)
 	{
+		ft_printf("------------------------------------------\n\n");
 		line = get_next_line(cub3d->map_fd);
 		if (line == NULL)
 			break ;
+		ft_printf("line: %s\n", line);
 		if (jump_empty(line, 0) >= 0)
 		{
 			ret = get_elements(line, cub3d);
+			ft_printf("get_elements: %d\n", ret);
 			if (ret == -1)
 				return (close(cub3d->map_fd), -3);
 			else if (ret == 1)
 				ret = get_map(cub3d, line);
+			ft_printf("ret:%d\n", ret);
 			if (ret != 0)
 				break ;
 		}
+		ft_printf("free line\n");
 		if (line)
 			free(line);
+		ft_printf("freed\n");
 		line = NULL;
+		ft_printf("NULL\n\n");
 	}
 	if (ret < 0)
 		return (close(cub3d->map_fd), ret);
--- src/map.c
+++ src/map.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 12:15:33 by ngordobi          #+#    #+#             */
-/*   Updated: 2025/04/22 13:04:25 by ngordobi         ###   ########.fr       */
+/*   Updated: 2025/04/23 12:51:09 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	get_new_map(t_cub3d *cub3d, char **new_map, char *line)
 		new_map[i] = ft_strdup(cub3d->map[i]);
 		i++;
 	}
-	new_map[i] = ft_strdup(line);
+	new_map[i] = ft_substr(line, 0, ft_strlen(line) - 2);
 	if (!new_map || !new_map[i])
 		return (free_matrix(new_map), -4);
 	new_map[i + 1] = NULL;
@@ -38,20 +38,26 @@ int	get_map(t_cub3d *cub3d, char *line)
 	{
 		cub3d->map = malloc(2 * sizeof(char *));
 		if (!cub3d->map)
-			return (-1	);
-		cub3d->map[0] = ft_strdup(line);
+			return (-1);
+		cub3d->map[0] = ft_substr(line, 0, ft_strlen(line) - 2);
 		if (!cub3d->map[0])
 			return (-4);
 		cub3d->map[1] = NULL;
+		ft_printf("\nfirst\n%m------------\n", cub3d->map);
 		i = 1;
 		return (0);
 	}
+	ft_printf("1\n");
 	new_map = malloc((i + 1) * (sizeof(char *)));
+	ft_printf("2\n");
 	if (!new_map)
 		return (-1);
+	ft_printf("3\n");
 	if (get_new_map(cub3d, new_map, line) != 0)
 		return (-1);
+	ft_printf("4\n");
 	cub3d->map = new_map;
+	ft_printf("\n%m------------\n", cub3d->map);
 	return (i++, 0);
 }
 
