1f7de050ff30aefef0fa47604463460a9a566cbe
Noah Gordobil Merino <ngordobi@c3r2s6.42urduliz.com>
2025-04-24 13:43:52 +0200

    gnl

--- libft/ft_strings/get_next_line.c
+++ libft/ft_strings/get_next_line.c
@@ -12,53 +12,104 @@
 
 #include "../../includes/libft.h"
 
-char	*ft_strdup2(char *line)
+char	*save_line(char	*buffer)
 {
-	char	*dup;
-	int		i = 0;
+	char	*line;
+	int		i;
 
-	while (line[i] != '\0')
-		i++;
-	dup = malloc((i + 1) * sizeof(char));
-	if (!dup || !line)
-		return (NULL);
 	i = 0;
-	while (line[i] != '\0')
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	if (!line)
+		return (free(buffer), NULL);
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
 	{
-		dup[i] = line[i];
+		line[i] = buffer[i];
 		i++;
 	}
-	dup[i] = '\0';
-	return (dup);
+	if (buffer[i] != '\0' && buffer[i] == '\n')
+		line[i] = '\n';
+	else
+		line[i] = '\0';
+	return (line);
+}
+
+char	*buffer_update(char *buffer)
+{
+	char	*temp_buff;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] != '\0' && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	temp_buff = ft_calloc((ft_strlen(buffer) - i++), sizeof(char));
+	if (!temp_buff)
+		return (free(buffer), NULL);
+	j = 0;
+	while (buffer[i] != '\0')
+		temp_buff[j++] = buffer[i++];
+	temp_buff[j] = '\0';
+	free (buffer);
+	return (temp_buff);
+}
+
+char	*append(char *buffer, char *temp_buff)
+{
+	char	*temp;
+
+	temp = ft_strjoin(buffer, temp_buff);
+	if (buffer)
+		free(buffer);
+	return (temp);
+}
+
+char	*read_text(int fd, char *buffer)
+{
+	char	*temp_buff;
+	int		read_chars;
+
+	if (!buffer)
+		buffer = ft_calloc(1, 1);
+	temp_buff = ft_calloc((1 + 1), sizeof(char));
+	if (!temp_buff)
+		return (free(buffer), NULL);
+	read_chars = 1;
+	while (read_chars != 0 && ft_strchr(buffer, '\n') == NULL)
+	{
+		read_chars = read(fd, temp_buff, 1);
+		if (read_chars < 0)
+		{
+			free(temp_buff);
+			return (free(buffer), NULL);
+		}
+		temp_buff[read_chars] = '\0';
+		buffer = append(buffer, temp_buff);
+	}
+	free(temp_buff);
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	char		buffer[10];
-	static int	rd_buff;
-	static int	i_buff;
-	char		line[5000];
-	char		i = 0;
+	static char	*buffer;
+	char		*line;
 
-	while (1)
-	{
-		if (i_buff >= rd_buff)
-		{
-			rd_buff = read(fd, buffer, 10);
-			i_buff = 0;
-			if (rd_buff <= 0)
-				break ;
-		}
-		if (line[i] == '\n')
-			break ;
-		
-		line[i] = buffer[i_buff];
-		i_buff++;
-		i++;
-	}
-	line[i] = '\0';
-	if (i = 0)
+	if (fd < 0)
 		return (NULL);
-	else
-		return (ft_strdup(line));
+	buffer = read_text(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = save_line(buffer);
+	buffer = buffer_update(buffer);
+	return (line);
 }
